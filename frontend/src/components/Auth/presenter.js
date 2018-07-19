a54a4babb8310021d1da20e53b723dd1c590e1e3
Yoonje Choi <yoonjechoi@gmail.com>
2018-07-19 15:52:14 +0900

    feat(auth): style auth component

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './styles.scss';
+import styles from './styles.scss';
 
 const Auth = (props, context) => (
-  <main>
+  <main className={styles.auth}>
+    <div className={styles.column}>
+      <img src={require("images/phone.png")} alt="Checkout our app. Is cool"/>
+    </div>
+    <div className={styles.column}>
+      <div className={`${styles.whiteBox} {styles.formBox}`}>
+        <img src={require("images/logo.png")} alt="Logo"/>
+      </div>
 
+    </div>
   </main>
 );
 
